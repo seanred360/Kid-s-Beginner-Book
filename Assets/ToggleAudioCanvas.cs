d84538bb3ef50c93442b77c90b5a8e8101843110
seanred360 <41050673+seanred360@users.noreply.github.com>
2020-08-29 06:58:36 +0800

    finished placing kbbsb audio buttons

---
+++ Assets/ToggleAudioCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAudioCanvas : MonoBehaviour
+{
+    GameObject audioPlayerCanvas;
+
+    private void Start()
+    {
+        audioPlayerCanvas = AudioPlayerManager.instance.audioPlayerCanvas;
+        audioPlayerCanvas.SetActive(false);
+    }
+
+    public void ToggleAudioPlayerCanvas()
+    {
+        audioPlayerCanvas.SetActive(!audioPlayerCanvas.activeSelf);
+    }
+}
