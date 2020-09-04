1ecd715278aeaad8b30ce4dbc9a5e37e40749dec
seanred360 <41050673+seanred360@users.noreply.github.com>
2020-09-04 14:28:33 +0800

    started adding swipe controls to mobile

---
+++ Assets/TouchSwiper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public float percentThreshold = .2f;
+    private int currentPage = 1;
+
+    MobileBook book;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        book = GetComponent<MobileBook>();
+    }
+    public void OnDrag(PointerEventData data)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
+        if (Mathf.Abs(percentage) >= percentThreshold)
+        {
+            if (percentage > 0)
+            {
+                book.FlipPageRight();
+            }
+            else if (percentage < 0)
+            {
+                book.FlipPageLeft();
+            }
+        }
+    }
+}
+
+
